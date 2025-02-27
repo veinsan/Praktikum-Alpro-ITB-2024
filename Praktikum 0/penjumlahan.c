d20763883a840cad776123e31ecd92b7dc0c26f1
Rian <157286510+veinsan@users.noreply.github.com>
2025-02-27 17:34:51 +0700

    Add files via upload

---
+++ Praktikum 0/penjumlahan.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    float hasil, x, y, z;
+    scanf("%f", &x);
+    scanf("%f", &y);
+    scanf("%f", &z);
+    hasil = x + y + z;
+    printf ("%f\n", hasil);
+    return 0;
+}
